1bab35a74c1e104539da682a6de3385a31943cc3
John Diego <johnoliveirasilva9@gmail.com>
2021-10-03 15:00:51 -0300

    Pass Args to Class Export_Xls

--- export_client.rb
+++ export_client.rb
@@ -1,24 +1,22 @@
 require 'byebug'
 require 'spreadsheet'
 require_relative 'connection'
-
+require_relative 'export_xls'
 
 client = Connection.new.open
-resultQuery = client.query("SELECT `clientes`.* FROM `clientes` INNER JOIN `atendimentosite_clientes` ON `atendimentosite_clientes`.`cliente_id` = `clientes`.`id` INNER JOIN `atendimentosites` ON `atendimentosites`.`id` = `atendimentosite_clientes`.`atendimentosite_id` WHERE `atendimentosites`.`id` = 720597")
+resultQuery = client.query("SELECT clientes.id, 
+                                   clientes.nome, 
+                                   clientes.email01, 
+                                   clientes.telefone01, 
+                                   clientes.telefone02 
+                            FROM `clientes` 
+                            INNER JOIN `atendimentosite_clientes` 
+                            ON `atendimentosite_clientes`.`cliente_id` = `clientes`.`id` 
+                            INNER JOIN `atendimentosites` 
+                            ON `atendimentosites`.`id` = `atendimentosite_clientes`.`atendimentosite_id` 
+                            WHERE `atendimentosites`.`id` = 720597")
 
 ################################# Export XLSX
 
-Spreadsheet.client_encoding = 'UTF-8'
-
-book = Spreadsheet::Workbook.new
-sheet1 = book.create_worksheet
-
-sheet1.row(0).concat %w{ID NOME EMAIL TEL01 TEL02}
-
-@row = []
-resultQuery.each_with_index do |val, index|
-  byebug
-  @row.push(sheet1.row(index + 1).push(val["id"], val["nome"], val["email01"], val["telefone01"], val["telfone02"]))
-end
-## Write the Excel File
-book.write('excel-file.xls')
+columns = resultQuery.first.keys
+ExportXls.new(columns, resultQuery, 'batatinha').process
